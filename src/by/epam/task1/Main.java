0d675293ed66bf61a90876f16f70b8dcc5487c5e
BlrSpy <roadtoti6dotka2@mail.ru>
2021-01-28 18:18:03 +0300

    Initial commit

---
+++ src/by/epam/task1/Main.java
@@ -0,0 +1,8 @@
+package by.epam.task1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(FirstTask.getSquareLastNumeral(1333));
+    }
+}
---
+++ src/by/epam/task8/Main.java
@@ -0,0 +1,8 @@
+package by.epam.task8;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(EighthTask.getValueFunc(5.5));
+    }
+}
